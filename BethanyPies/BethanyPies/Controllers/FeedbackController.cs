ae6ab5a1db4d297820eaf143d90518b80e1067e9
Romeo Rego <Romeo.Rego@unit4.com>
2019-02-11 17:59:09 +0100

    Added validation to Feedback

--- BethanyPies/BethanyPies/Controllers/FeedbackController.cs
+++ BethanyPies/BethanyPies/Controllers/FeedbackController.cs
@@ -22,8 +22,13 @@ namespace BethanyPies.Controllers
         [HttpPost]
         public IActionResult Index(Feedback feedback)
         {
-            _feedbackRepository.AddFeedback(feedback);
-            return RedirectToAction("FeedbackComplete");
+            if (ModelState.IsValid)
+            {
+                _feedbackRepository.AddFeedback(feedback);
+                return RedirectToAction("FeedbackComplete");
+            }
+
+            return View(feedback);
         }
 
         public IActionResult FeedbackComplete()
